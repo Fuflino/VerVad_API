8e94bd4ecda7d6cba2ec48e47e884e9cd01191a2
Raakost <jeanettesydboege@gmail.com>
2017-12-05 15:34:43 +0100

    ..

--- DAL/Contexts/DbInit.cs
+++ DAL/Contexts/DbInit.cs
@@ -36,7 +36,7 @@ namespace DAL.Contexts
 
             var transEN = new TranslationLanguage()
             {
-                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non scelerisque nibh. Vestibulum ante ipsum " +
+                Description = "ENGELSK: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non scelerisque nibh. Vestibulum ante ipsum " +
                 "primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur vel purus ultricies mauris fringilla rhoncus." +
                 "Duis a vehicula nunc, a sagittis leo. Etiam tempor faucibus orci ac cursus. Vestibulum ante ipsum primis in faucibus" +
                 "orci luctus et ultrices posuere cubilia Curae; Sed vestibulum neque vitae nisi blandit commodo.Maecenas varius," +
@@ -46,8 +46,7 @@ namespace DAL.Contexts
             };
             var transDK = new TranslationLanguage()
             {
-
-                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non scelerisque nibh. Vestibulum ante ipsum " +
+                Description = "DANSK: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non scelerisque nibh. Vestibulum ante ipsum " +
                 "primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur vel purus ultricies mauris fringilla rhoncus." +
                 "Duis a vehicula nunc, a sagittis leo. Etiam tempor faucibus orci ac cursus. Vestibulum ante ipsum primis in faucibus" +
                 "orci luctus et ultrices posuere cubilia Curae; Sed vestibulum neque vitae nisi blandit commodo.Maecenas varius," +
@@ -56,9 +55,20 @@ namespace DAL.Contexts
                 Language = LanguageDA,
             };
 
+            var transDE = new TranslationLanguage()
+            {
+                Description = "TYSK Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non scelerisque nibh. Vestibulum ante ipsum " +
+                "primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur vel purus ultricies mauris fringilla rhoncus." +
+                "Duis a vehicula nunc, a sagittis leo. Etiam tempor faucibus orci ac cursus. Vestibulum ante ipsum primis in faucibus" +
+                "orci luctus et ultrices posuere cubilia Curae; Sed vestibulum neque vitae nisi blandit commodo.Maecenas varius," +
+                " dolor at commodo.",
+                Title = "Welt Ziele eller noget..",
+                Language = LanguageDE,
+            };
+
             var TranslatedTexts = new Translation()
             {
-                TranslatedTexts = new List<TranslationLanguage> { transDK, transEN }
+                TranslatedTexts = new List<TranslationLanguage> { transDK, transEN, transDE }
             };
             context.Texts.Add(TranslatedTexts);
 
@@ -72,56 +82,37 @@ namespace DAL.Contexts
             //Artwork
             var Artwork = new Artwork()
             {
+                Id = 1,
                 Artist = "Bart 4c",
                 Translation = TranslatedTexts,
                 ImgUrl = "https://placeimg.com/600/400/nature"
 
             };
-            var Artwork2 = new Artwork()
-            {
-                Artist = "Bart 4c",
-                Translation = TranslatedTexts,
-                ImgUrl = "https://placeimg.com/600/400/nature"
 
-            };
             //Landart
             var Landart = new LandArt()
             {
+                Id = 1,
                 Translation = TranslatedTexts,
                 ImgUrl = "https://placeimg.com/600/400/nature"
 
             };
-            var Landart2 = new LandArt()
-            {
-                Translation = TranslatedTexts,
-                ImgUrl = "https://placeimg.com/600/400/nature"
 
-            };
 
             //Childrens Texts
             var ChildrensTexts = new ChildrensText()
             {
+                Id = 1,
                 Author = "Pippi 8c",
                 Translation = TranslatedTexts
             };
 
-            var ChildrensTexts2 = new ChildrensText()
-            {
-                Author = "Pippi 8c",
-                Translation = TranslatedTexts
-            };
+
 
             //AudioVideo
             var audioVideo = new AudioVideo()
             {
-                AudioURL = "https://www.dropbox.com/s/ewkmod3sbhw71ia/listener%20-%20Wooden%20Heart%20-%2001%20You%20have%20never%20lived%20because%20you%20have%20never%20died.mp3?dl=1",
-                VideoURL = "https://www.youtube.com/embed/RpqVmvMCmp0",
-                SongTitle = "Bum Bum Bla Bla..",
-                SongArtist = "Shakira",
-                Translation = TranslatedTexts
-            };
-            var audioVideo2 = new AudioVideo()
-            {
+                Id = 1,
                 AudioURL = "https://www.dropbox.com/s/ewkmod3sbhw71ia/listener%20-%20Wooden%20Heart%20-%2001%20You%20have%20never%20lived%20because%20you%20have%20never%20died.mp3?dl=1",
                 VideoURL = "https://www.youtube.com/embed/RpqVmvMCmp0",
                 SongTitle = "Bum Bum Bla Bla..",
@@ -129,33 +120,36 @@ namespace DAL.Contexts
                 Translation = TranslatedTexts
             };
 
+
             //Global Goal
             var GG = new GlobalGoal()
             {
+                Id = 1,
                 Latitude = 12.3045,
                 Longitude = 34.5566,
                 Translation = TranslatedTexts,
                 ImgURL = "https://placeimg.com/600/400/nature",
                 IsPublished = true,
 
-                ChildrensTexts = new List<ChildrensText> { ChildrensTexts, ChildrensTexts2 },
-                Artworks = new List<Artwork> { Artwork, Artwork2 },
-                LandArts = new List<LandArt> { Landart, Landart2 },
+                ChildrensTexts = new List<ChildrensText> { ChildrensTexts },
+                Artworks = new List<Artwork> { Artwork },
+                LandArts = new List<LandArt> { Landart },
                 AudioVideo = audioVideo
             };
 
             var GG2 = new GlobalGoal()
             {
+                Id = 2,
                 Latitude = 12.3045,
                 Longitude = 34.5566,
                 Translation = TranslatedTexts,
                 ImgURL = "https://placeimg.com/600/400/nature",
                 IsPublished = false,
 
-                ChildrensTexts = new List<ChildrensText> { ChildrensTexts, ChildrensTexts2 },
+                ChildrensTexts = new List<ChildrensText> { },
                 Artworks = new List<Artwork> { },
                 LandArts = new List<LandArt> { },
-                AudioVideo = audioVideo2
+                AudioVideo = audioVideo
             };
 
 
